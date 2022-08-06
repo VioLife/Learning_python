6a17802962904c2e8cb4a3bcaec376b249eb8f6d
Violetta <forbes.life@mail.ru>
2022-08-06 21:52:01 +0300

    Update logger

--- Mobile_Guide/logger.py
+++ Mobile_Guide/logger.py
@@ -1,8 +1,8 @@
 from datetime import datetime as dt
 
 def phone_number(st):
-    time = dt.now().strftime('%H:%M')
+    time = dt.today().strftime('%D/%m/%y.%H.%M')
     with open('log.csv', 'a') as p_file:
-        p_file.write('{}; Number_Phone: {}\n'
+        p_file.write('{}; New_contact: {}\n'
                      .format(time,st))
         
